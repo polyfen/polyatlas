c7d6baa4cd5238e97aee31b5aef5fe7e9fe91ae8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-19 17:37:06 +0100

    Add title and paragraph to donation module

--- sections/donations.php
+++ sections/donations.php
@@ -1,1 +1,5 @@
+<div id="donations-banner">
+<h5>Your support is appreciated!</h5>
+<p>Help us keep polyatlas.wiki running and growing.</p>
 <button id="donations-button" onclick="window.open('https://donate.stripe.com/fZe4ih3hT6Prg0g000','_blank')">Send a Donation</button>
+</div>
