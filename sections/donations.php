986b694032da918fd0588ace83b603b3cb5fee3e
Boris Hrnčić <borishrncic@users.noreply.github.com>
2022-11-24 19:20:36 +0100

    Updated donations copy

--- sections/donations.php
+++ sections/donations.php
@@ -1,5 +1,5 @@
 <div id="donations-banner">
-<h5>Your support is appreciated!</h5>
-<p>Help us keep polyatlas.wiki running and growing.</p>
+<h5>Help us keep design education accessible</h5>
+<p>Your support keeps our site running and growing.</p>
 <button id="donations-button" onclick="window.open('https://donate.stripe.com/fZe4ih3hT6Prg0g000','_blank')">Send a Donation</button>
 </div>
