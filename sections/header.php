7ea44d60cc9d1126f8b67f4b8f2f49263fc896a2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-03-01 13:26:22 +0100

    Fixed change of theme bug

--- sections/header.php
+++ sections/header.php
@@ -18,9 +18,9 @@
 
   <a title="Version" id="version-button-active" class="d-none"  onclick="document.getElementById('version-banner').className='d-none';document.getElementById('version-button-active').className='d-none';document.getElementById('version-button-inactive').className='version-button inactive-header-button';">20 Nov 2022</a>
 
-  <a title="Change Theme" id="change-to-dark" class="change-theme-button" onclick="document.getElementById('web-body').className='dark-theme';document.getElementById('change-to-dark').className='change-theme-button d-none';document.getElementById('change-to-light').className='change-theme-button';document.getElementById('header').className='dark-theme';"><i class="fa-solid fa-lightbulb"></i></a>
+  <a title="Change Theme" id="change-to-dark" class="change-theme-button" onclick="document.body.className='dark-theme';document.getElementById('change-to-dark').className='change-theme-button d-none';document.getElementById('change-to-light').className='change-theme-button';document.getElementById('header').className='dark-theme';"><i class="fa-solid fa-lightbulb"></i></a>
 
-  <a title="Change Theme" id="change-to-light" class="change-theme-button d-none" onclick="document.getElementById('web-body').className='light-theme';document.getElementById('change-to-light').className='change-theme-button d-none';document.getElementById('change-to-dark').className='change-theme-button';document.getElementById('header').className='light-theme';"><i class="fa-solid fa-lightbulb"></i></a>
+  <a title="Change Theme" id="change-to-light" class="change-theme-button d-none" onclick="document.body.className='light-theme';document.getElementById('change-to-light').className='change-theme-button d-none';document.getElementById('change-to-dark').className='change-theme-button';document.getElementById('header').className='light-theme';"><i class="fa-solid fa-lightbulb"></i></a>
 
   <a href="javascript:void(0)" onclick="javascript:toggleFullScreen()" class="fullscreen-button" title="Fullscreen"><i class="fas fa-expand-arrows-alt"></i></a>
 
