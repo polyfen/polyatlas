1874f0d3d1eda53814f337a59f74a6f5f3fe2456
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-03-01 13:49:44 +0100

    Alternative typefaces test

--- sections/head.php
+++ sections/head.php
@@ -67,7 +67,24 @@
   <!-- CSS Page-Specific-->
 
   <!-- FONTS -->
-  <link rel="stylesheet" href="https://use.typekit.net/equ5vqy.css">
+   <!--
+  /**
+  * @license
+  * MyFonts Webfont Build ID 568563
+  *
+  * The fonts listed in this notice are subject to the End User License
+  * Agreement(s) entered into by the website owner. All other parties are
+  * explicitly restricted from using the Licensed Webfonts(s).
+  *
+  * You may obtain a valid license from one of MyFonts official sites.
+  * http://www.fonts.com
+  * http://www.myfonts.com
+  * http://www.linotype.com
+  *
+  */
+  -->
+  <link rel="stylesheet" type="text/css" href="libs/fonts/Biotif.css">
+  <link rel="stylesheet" href="https://use.typekit.net/aba8ecz.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
   <!-- Google Tag Manager -->
