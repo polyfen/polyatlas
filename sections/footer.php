dc02c203342dcf208213c318f1ae4b5715e32227
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-06-25 15:29:55 +0200

    Added UTM to TPG logo

--- sections/footer.php
+++ sections/footer.php
@@ -4,12 +4,12 @@
     <div id="tpg" onmouseover="document.getElementById('tpg-dark').className='d-none';document.getElementById('tpg-light').className='tpg-logo d-block';"
 onmouseout="document.getElementById('tpg-dark').className='tpg-logo d-block';document.getElementById('tpg-light').className='d-none';">
       <div class="tpg-logo" id="tpg-dark">
-        <a href="https://thepolygroup.co" target="_blank">
+        <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
           <img src="imgs/the-poly-group_low.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-dark">
         </a>
       </div>
       <div class="d-none" id="tpg-light">
-        <a href="https://thepolygroup.co" target="_blank">
+        <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
           <img src="imgs/the-poly-group.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-light">
         </a>
       </div>
@@ -20,12 +20,12 @@ onmouseout="document.getElementById('tpg-dark').className='tpg-logo d-block';doc
     <div id="tpg" onmouseover="document.getElementById('tpg-dark-negative').className='d-none';document.getElementById('tpg-light-negative').className='tpg-logo d-block';"
 onmouseout="document.getElementById('tpg-dark-negative').className='tpg-logo d-block';document.getElementById('tpg-light-negative').className='d-none';">
       <div class="tpg-logo" id="tpg-dark-negative">
-        <a href="https://thepolygroup.co" target="_blank">
+        <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
           <img src="imgs/the-poly-group_negative_low.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-dark">
         </a>
       </div>
       <div class="d-none" id="tpg-light-negative">
-        <a href="https://thepolygroup.co" target="_blank">
+        <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
           <img src="imgs/the-poly-group_negative.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-light">
         </a>
       </div>
