1a29885d1c2cd97ca5b8f08f26aa535b5b4e26b0
xxiicode <leandrobilokapic@gmail.com>
2023-09-15 16:18:32 +0200

    logo > footer > changed to new polyfen-group

--- sections/footer.php
+++ sections/footer.php
@@ -1,8 +1,8 @@
 <footer>
-  <div id="tpg">
+  <div id="the-poly-group">
     <a href="https://thepolyfengroup.com?utm_source=polyfen&utm_medium=referral" target="_blank">
-      <img src="imgs/the-polyfen-group-low.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById('tpg-logo-low').className='d-none';document.getElementById('tpg-logo').className='tpg-logo';">
-      <img src="imgs/the-polyfen-group.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById('tpg-logo').className='d-none';document.getElementById('tpg-logo-low').className='tpg-logo';">
+      <img src="imgs/the-polyfen-group-low-alt.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById('tpg-logo-low').className='d-none';document.getElementById('tpg-logo').className='tpg-logo';">
+      <img src="imgs/the-polyfen-group-alt.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById('tpg-logo').className='d-none';document.getElementById('tpg-logo-low').className='tpg-logo';">
     </a>
   </div>
 
