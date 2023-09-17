5ed67ef9705a68d42f1e32c4a9f8e533e04fe148
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-17 18:08:53 +0200

    Updated meta author

--- sections/head.php
+++ sections/head.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta property="og:type" content="website"/>
-  <meta name="author"  content="The Poly Group" />
+  <meta name="author"  content="The Polyfen Group LLC" />
 
 
   <!-- PAGE-SPECIFIC META start -->
