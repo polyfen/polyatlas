39d255274c5cb42b242abbb96913671e1f90a748
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-17 02:15:46 +0200

    GTM Container Hotfix

--- brand-identity-design.php
+++ brand-identity-design.php
@@ -8,7 +8,7 @@
   <body id="brand-identity-design-container" class="light-theme">
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8K76NG"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBRK5MR5"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
