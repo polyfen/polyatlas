1b56bbf506d21493fb4bb9afcccde6b211db06ab
xxiicode <leandrobilokapic@gmail.com>
2023-09-18 12:52:56 +0200

    dark version

--- sections/footer.php
+++ sections/footer.php
@@ -6,35 +6,12 @@
     </a>
   </div>
 
-  <!-- 	
-    <div id="the-poly-group">
-      <div id="tpg" onmouseover="document.getElementById('tpg-dark').className='d-none';document.getElementById('tpg-light').className='tpg-logo d-block';" onmouseout="document.getElementById('tpg-dark').className='tpg-logo d-block';document.getElementById('tpg-light').className='d-none';">
-        <div class="tpg-logo" id="tpg-dark">
-          <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
-            <img src="imgs/the-poly-group_low.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-dark">
-          </a>
-        </div>
-        <div class="d-none" id="tpg-light">
-          <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
-            <img src="imgs/the-poly-group.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-light">
-          </a>
-        </div>
-      </div>
-    </div>
+  <div id="the-poly-group-negative">
+    <a href="https://thepolyfengroup.com?utm_source=polyfen&utm_medium=referral" target="_blank">
+      <img src="imgs/the-polyfen-group-low.svg" id="tpg-logo-low" class="tpg-logo" onmouseover="document.getElementById('tpg-logo-low').className='d-none';document.getElementById('tpg-logo').className='tpg-logo';">
+      <img src="imgs/the-polyfen-group.svg" id="tpg-logo" class="d-none" onmouseout="document.getElementById('tpg-logo').className='d-none';document.getElementById('tpg-logo-low').className='tpg-logo';">
+    </a>
+  </div>
+    
 
-    <div id="the-poly-group-negative">
-      <div id="tpg" onmouseover="document.getElementById('tpg-dark-negative').className='d-none';document.getElementById('tpg-light-negative').className='tpg-logo d-block';" onmouseout="document.getElementById('tpg-dark-negative').className='tpg-logo d-block';document.getElementById('tpg-light-negative').className='d-none';">
-        <div class="tpg-logo" id="tpg-dark-negative">
-          <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
-            <img src="imgs/the-poly-group_negative_low.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-dark">
-          </a>
-        </div>
-        <div class="d-none" id="tpg-light-negative">
-          <a href="https://thepolygroup.co?utm_source=polyatlas&utm_medium=referral" target="_blank">
-            <img src="imgs/the-poly-group_negative.svg" height="35px" width="auto" style="box-shadow:none;" id="tpg-light">
-          </a>
-        </div>
-      </div>
-    </div>
-  -->
 </footer>
